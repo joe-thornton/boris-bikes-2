d3ab837b9d72cbd22e2ef3d351a3d777f738f13f
Felix Taljaard <felixtaljaard@Felixs-MacBook-Air.local>
2022-02-22 17:21:12 +0000

    Step 8

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,8 @@
 require 'docking_station.rb'
 
 describe DockingStation do
+    docking_station = DockingStation.new
+    it "should respond to release_bike" do
+        expect(docking_station).to respond_to(:release_bike)
+    end
 end
