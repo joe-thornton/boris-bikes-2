cbe82a43c9089052c2e338ac8cd93c4aa18f907b
Joe Thornton <thornton.jp@gmail.com>
2022-02-25 18:02:32 +0000

    fexercise 19 completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,14 +6,14 @@ describe DockingStation do
     end
 
     it 'should release a working bike' do
-        bike = Bike.new
+        bike = double(:bike)
         subject.dock(bike)
         expect(bike.working?).to(eq(true))
         expect(subject.release_bike).to eq bike
     end
 
     it 'should dock a bike' do
-        bike = Bike.new
+        bike = double(:bike)
         expect(subject.dock(bike)).to eq [bike]
     end
 
@@ -24,8 +24,8 @@ describe DockingStation do
 
     it 'should raise an error when trying to dock a bike where the docking station is full' do
         station = DockingStation.new
-        station.capacity.times { station.dock(Bike.new) }
-        expect { station.dock(Bike.new) }.to raise_error('Dock full error')
+        station.capacity.times { station.dock(double(:bike)) }
+        expect { station.dock(double(:bike)) }.to raise_error('Dock full error')
     end
 
     it 'should allow the system maintainer to set the capacity of docking stations' do
@@ -38,13 +38,13 @@ describe DockingStation do
 
     it 'should have a variable capacity' do
         station = DockingStation.new(50)
-        50.times { station.dock(Bike.new) }
-        expect{ station.dock(Bike.new) }.to raise_error 'Dock full error'
+        50.times { station.dock(double(:bike)) }
+        expect{ station.dock(double(:bike)) }.to raise_error 'Dock full error'
     end
 
     it 'should not release broken bikes' do
         station = DockingStation.new
-        bike = Bike.new
+        bike = double(:bike)
         bike.report_broken
         station.dock(bike)
         expect(station.release_bike).to eq nil
