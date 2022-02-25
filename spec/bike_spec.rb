11191a334e74e3d13a3bd2a68b2475ec6bc68f83
Adam Woodcock <adam.woodcock98@gmail.com>
2022-02-25 15:49:32 +0000

    Created basic tests for reporting a broken bike and implemented code to satisfy conditions

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,4 +4,8 @@ describe Bike do
   it "should respond to working?" do
     expect(subject).to respond_to(:working?)
   end
+
+  it 'should respond to report_broken' do
+    expect(subject).to respond_to(:report_broken)
+  end
 end
